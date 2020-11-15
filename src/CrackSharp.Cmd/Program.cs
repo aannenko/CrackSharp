7ea95f1f4907b2357bd268f4e104ef367f7604d8
Andrii Annenko <roxton.public@gmail.com>
2020-11-15 15:26:25 +0100

    Make CrackSharp.Cmd a top-level program

--- src/CrackSharp.Cmd/Program.cs
+++ src/CrackSharp.Cmd/Program.cs
@@ -1,33 +1,23 @@
 ﻿using System;
-using System.Threading.Tasks;
 using CrackSharp.Core.Des;
 
-namespace CrackSharp.Cmd
+const int MaxWordLength = 5;
+const string Chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+if (args.Length != 1)
 {
-    class Program
-    {
-        private const int MaxWordLength = 5;
-        private const string Chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-        static async Task<int> Main(string[] args)
-        {
-            if (args.Length != 1)
-            {
-                Console.WriteLine("Usage: ./crack hash\n");
-                return 1;
-            }
-
-            try
-            {
-                Console.WriteLine($"{await DesDecryptor.DecryptAsync(args[0], MaxWordLength, Chars)}\n");
-                return 0;
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
-
-            return 1;
-        }
-    }
+    Console.WriteLine($"Usage: ./crack hash");
+    return 1;
 }
+
+try
+{
+    Console.WriteLine($"{await DesDecryptor.DecryptAsync(args[0], MaxWordLength, Chars)}\n");
+    return 0;
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
+
+return 1;
